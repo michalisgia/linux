8a2c3b1d17f72c45e9851469821d6f33d70c4e3b
Tomasz Jeznach <tjeznach@rivosinc.com>
2023-07-17 18:24:47 -0700

    iommu/riscv: Add SVA with PASID/ATS/PRI support
    Introduces SVA (Shared Virtual Address) for RISC-V IOMMU, with
    ATS/PRI services for capable devices.
    
    Co-developed-by: Sebastien Boeuf <seb@rivosinc.com>
    Signed-off-by: Sebastien Boeuf <seb@rivosinc.com>
    Signed-off-by: Tomasz Jeznach <tjeznach@rivosinc.com>

--- drivers/iommu/riscv/iommu.h
+++ drivers/iommu/riscv/iommu.h
@@ -17,11 +17,14 @@
 #include <linux/iova.h>
 #include <linux/io.h>
 #include <linux/idr.h>
+#include <linux/mmu_notifier.h>
 #include <linux/list.h>
 #include <linux/iommu.h>
 #include <linux/io-pgtable.h>
+#include <linux/mmu_notifier.h>
 
 #include "iommu-bits.h"
+#include "../iommu-sva.h"
 
 #define IOMMU_PAGE_SIZE_4K     BIT_ULL(12)
 #define IOMMU_PAGE_SIZE_2M     BIT_ULL(21)
@@ -76,6 +79,9 @@ struct riscv_iommu_device {
 	unsigned int ddt_mode;
 	bool ddtp_in_iomem;
 
+	/* I/O page fault queue */
+	struct iopf_queue *pq_work;
+
 	/* hardware queues */
 	struct riscv_iommu_queue cmdq;
 	struct riscv_iommu_queue fltq;
@@ -91,12 +97,15 @@ struct riscv_iommu_domain {
 	struct io_pgtable pgtbl;
 
 	struct list_head endpoints;
+	struct list_head notifiers;
 	struct mutex lock;
+	struct mmu_notifier mn;
 	struct riscv_iommu_device *iommu;
 
 	bool is_32bit;		/* SXL/GXL 32-bit modes enabled */
 	unsigned int mode;	/* RIO_ATP_MODE_* enum */
 	unsigned int pscid;	/* RISC-V IOMMU PSCID */
+	ioasid_t pasid;		/* IOMMU_DOMAIN_SVA: Cached PASID */
 
 	pgd_t *pgd_root;	/* page table root pointer */
 };
@@ -108,10 +117,16 @@ struct riscv_iommu_endpoint {
 	unsigned int domid;			/* PCI domain number, segment */
 	struct rb_node node;			/* device tracking node (lookup by devid) */
 	struct riscv_iommu_dc *dc;		/* device context pointer */
+	struct riscv_iommu_pc *pc;		/* process context root, valid if pasid_enabled is true */
 	struct riscv_iommu_device *iommu;	/* parent iommu device */
 
 	struct mutex lock;
 	struct list_head domain;		/* endpoint attached managed domain */
+
+	/* end point info bits */
+	unsigned int pasid_bits;
+	unsigned int pasid_feat;
+	bool pasid_enabled;
 };
 
 /* Helper functions and macros */
